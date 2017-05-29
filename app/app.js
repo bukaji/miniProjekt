b2f5e76db484bb0e927f8daa3eba01275f7c42c5
bukaji <bukjai@gmail.com>
2017-05-29 23:22:59 +0200

    Complete bisection

--- app/app.js
+++ app/app.js
@@ -1,11 +1,31 @@
-function f(x) {
-    return Math.pow(x,3)-2*Math.pow(x,2)+4*x-1; // x^3-2*x^2+4*x-1
+function f(fun,x) {
+    //return Math.pow(x,3)-2*Math.pow(x,2)+4*x-1; // x^3-2*x^2+4*x-1
+    return eval(fun);
 }
 
 
-function bisekcja(a,b)
+function bisekcja(fun,a,b)
 {
-    return 0;
+    var eps=0.001;
+    var x0;
+    var fa;
+    var fb;
+    var fmiddle;
+
+    fa = f(fun,a);
+    fb = f(fun,b);
+
+    while(Math.abs(a - b) > eps){
+        x0 = (a + b) / 2;
+        fmiddle = f(fun,x0);
+        if(fa * fmiddle < 0) {
+            b = x0;
+        } else {
+            a = x0;
+            fa = fmiddle;
+        }
+    }
+    return parseFloat(x0.toFixed(3));
 }
 
 module.exports = {
