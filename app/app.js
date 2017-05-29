63f861f4192c17b1c02850fe264c9b76f1fe7d8d
bukaji <bukjai@gmail.com>
2017-05-29 22:39:14 +0200

    add F(x)

--- app/app.js
+++ app/app.js
@@ -1,8 +1,14 @@
-function bisekcja( stringFunction, )
+function f(x) {
+    return Math.pow(x,3)-2*Math.pow(x,2)+4*x-1; // x^3-2*x^2+4*x-1
+}
+
+
+function bisekcja(a,b)
 {
     return 0;
 }
 
 module.exports = {
+    f:f,
     bisekcja: bisekcja
 };
